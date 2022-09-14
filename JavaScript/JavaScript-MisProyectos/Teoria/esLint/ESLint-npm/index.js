2011ca05ab79a93a1acf6d1471bf22c4bfafa176
hernanguaragna <hernanguaragna@gmail.com>
2022-09-14 17:04:47 -0300

    hwk eslint

---
+++ JavaScript/JavaScript-MisProyectos/Teoria/esLint/ESLint-npm/index.js
@@ -0,0 +1,16 @@
+//en este archivo voy a trabajar con ESLint
+const nombre =  "hernan";
+
+/*eslint-disable */
+const persona2 = 'Juan'
+
+/*eslint-enable */
+
+const nuevoString = 'Estoy probando el eslint enable y disable'//eslint-disable-line
+
+/*eslint-disable-next-line */
+const ultimoEjemplo =  'Este es el ultimo ejemplo'
+
+const esteSiEsElUltimo = "the last one";
+
+
---
+++ JavaScript/JavaScript-MisProyectos/hwk-ESLint/index.js
@@ -0,0 +1,5 @@
+
+let str_sng = "Hola soy un texto con comillas simples"
+let str_dbl = "Hola soy un texto con comillas dobles"
+let str_backticks = "Hola esto es un string con backticks"
+
