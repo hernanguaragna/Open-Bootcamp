e72c159e476032999ec339ede51ae248935ef416
hernanguaragna <hernanguaragna@gmail.com>
2022-12-21 13:44:59 -0300

    inicialice el repo de ts

---
+++ TypeScript/Ejercicios1/build/index.js
@@ -0,0 +1,22 @@
+// Esto un comentario en TS
+/**
+ * Esto es un comentario
+ * Multilínea
+*/
+
+"use strict";
+console.log("Hola Martín");
+console.log("Adiós Martín");
+
+//Declaraciones de Variables
+
+const nombre = "Martin";
+const email = "hernanguaragna@gmail.com"
+
+
+ console.log("Hola "+ nombre);
+ console.log("Qué tal?", nombre, "?");
+ console.log(`Cómo han ido las vacaciones, ${nombre}?`)
+
+ console.log(`email de ${nombre} es ${email}`)
+
