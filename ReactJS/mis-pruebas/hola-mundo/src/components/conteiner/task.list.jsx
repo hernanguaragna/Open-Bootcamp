8e7d2f6aeeeead57a2e55504b28a3ed348e8b31e
hernanguaragna <hernanguaragna@gmail.com>
2023-01-05 14:20:42 -0300

    task.ist done

--- ReactJS/mis-pruebas/hola-mundo/src/components/conteiner/task.list.jsx
+++ ReactJS/mis-pruebas/hola-mundo/src/components/conteiner/task.list.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import { LEVELS } from '../../models/levels.enum';
+import { Task } from '../../models/task.class';
+import TaskComponent from '../pures/task';
 
 
 const TaskListComponent = () => {
-    defaultT
+   const defaultTask = new Task("Example", "Default description", false,LEVELS.NORMAL);
+
     return (
         <div>
-            
+            <div>
+                Your Task:
+            </div>
+            {/*  TODO aplicar un for/map para renderizar una lista */}
+            <TaskComponent Task={defaultTask}> </TaskComponent>
         </div>
     );
 };
@@ -15,4 +22,4 @@ const TaskListComponent = () => {
 
 
 
-export default TaskList;
+export default TaskListComponent;
