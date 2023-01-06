f07e8b9f566e136ef153fb954f24a0c2a98a93d3
hernanguaragna <hernanguaragna@gmail.com>
2023-01-06 00:58:01 -0300

    elimine la hwk3

--- ReactJS/mis-pruebas/hola-mundo/src/App.js
+++ ReactJS/mis-pruebas/hola-mundo/src/App.js
@@ -1,28 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 //import Greeting from './components/pures/greeting';
 //import GreetingF from './components/pures/greetingF';
 import TaskListComponent from './components/conteiner/task.list';
-import ComponenteA from './components/pures/forms/componenteA';
+
 
 function App() {
-  const contactoPrueba = {
-    nombre: 'Cosme',
-    apellido: 'Fulanito',
-    email: 'cosmefulanito@gmail.com',
-    conectado: false,
-  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
        {/* !aca voy a importar el componente Greeting */}
        {/* <Greeting name="Hernán"/> */}
        {/* Componente de ejemplo funcional */}
        {/* <GreetingF name={"Hernán"} /> */}
        {/* Componente de Listado de Tareas */}
        <TaskListComponent />
-       <ComponenteA contacto={contactoPrueba} />
+       
       </header>
     </div>
   );
