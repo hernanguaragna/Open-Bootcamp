cf24ea8cfe7e1fd420677e366c2ba71e47d5b7c8
hernanguaragna <hernanguaragna@gmail.com>
2023-01-17 10:28:21 -0300

    creando component didmount

---
+++ ReactJS/mis-pruebas/hola-mundo/src/hooks/livecyrcle/DidUpdate.jsx
@@ -0,0 +1,31 @@
+/**
+ * *Ejempo de uso de metodo componentDidUpdate en componente
+ * *clase y en componente funcional.
+ */
+
+import React, { Component } from 'react';
+
+export class DidUpdate extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+
+export const DidUpdate = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+
+
+
+
