1fcace0610177a537981e786d1a03642fb54e5c9
hernanguaragna <hernanguaragna@gmail.com>
2023-01-18 11:34:02 -0300

    cambios menores

--- ReactJS/mis-pruebas/hwks/hwk_4-5-6/componenteFuncional.jsx
+++ ReactJS/mis-pruebas/hwks/hwk_4-5-6/componenteFuncional.jsx
@@ -1,44 +1,43 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 // import ‘../../styles/clock.scss’;
 
 const ClockFuncional = () => {
-    const defaultState = {
-      fecha: new Date(),
-      edad: 0,
-      nombre: 'Martín',
-      apellidos: 'San José',
-    };
-  
-    const [user, setUser] = useState(defaultState);
-  
-    useEffect(() => {
-      const intervalAge = setInterval(() => {
-        actualiceUser();
-      }, 1000);
-      return () => {
-        clearInterval(intervalAge);
-      };
-    });
-  
-    const actualiceUser = () => {
-      return setUser({
-        ...user,
-        edad: user.edad + 1,
-        
-      });
-    };
-    return (
-      <div>
-        <h2>
-          Hora Actual:
-          {user.fecha.toLocaleTimeString()}
-        </h2>
-        <h3>
-          {user.nombre} {user.apellidos}
-        </h3>
-        <h1>Edad: {user.edad}</h1>
-      </div>
-    );
-  };
-  
-  export default ClockFuncional;
+	const defaultState = {
+		fecha: new Date(),
+		edad: 0,
+		nombre: "Martín",
+		apellidos: "San José",
+	};
+
+	const [user, setUser] = useState(defaultState);
+
+	useEffect(() => {
+		const intervalAge = setInterval(() => {
+			actualiceUser();
+		}, 1000);
+		return () => {
+			clearInterval(intervalAge);
+		};
+	});
+
+	const actualiceUser = () => {
+		return setUser({
+			...user,
+			edad: user.edad + 1,
+		});
+	};
+	return (
+		<div>
+			<h2>
+				Hora Actual:
+				{user.fecha.toLocaleTimeString()}
+			</h2>
+			<h3>
+				{user.nombre} {user.apellidos}
+			</h3>
+			<h1>Edad: {user.edad}</h1>
+		</div>
+	);
+};
+
+export default ClockFuncional;
