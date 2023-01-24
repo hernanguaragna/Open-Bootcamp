7e322cd8c3e2d51f147f12ffd49a630257bf2e15
hernanguaragna <hernanguaragna@gmail.com>
2023-01-24 15:46:34 -0300

    add new views

---
+++ ReactJS/mis-pruebas/hola-mundo/src/pages/tasks/TaskPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TaskListComponent from "./TasckListComponent";
+const TaskPage = () => {
+    return (
+        <div>
+            <TaskListComponent /> 
+        </div>
+    );
+}
+
+export default TaskPage;
