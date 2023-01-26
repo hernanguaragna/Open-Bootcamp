d31efbe65fe06100ac31a20c01fa5dc1eae1b74c
hernanguaragna <hernanguaragna@gmail.com>
2023-01-26 12:03:07 -0300

    hwk done

--- ReactJS/mis-pruebas/hwks/hwk_10-11-12/src/App.js
+++ ReactJS/mis-pruebas/hwks/hwk_10-11-12/src/App.js
@@ -1,14 +1,15 @@
-
+import React from "react";
+import ColorChanger from "./components/colorChanger.jsx"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+		<div className="App">
+			<header className="App-header">
+				<ColorChanger />
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ ReactJS/mis-pruebas/hwks/hwk_10-11-12/src/components/colorChanger.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function ColorChanger() {
+  const [color, setColor] = useState('black');
+
+  function handleMouseEnter() {
+    changeColor();
+  }
+
+  function handleDoubleClick() {
+    setColor('black');
+  }
+
+  function changeColor() {
+    setColor(`rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`);
+    setTimeout(changeColor, 500);  
+  }
+
+  return (
+    <div
+      style={{ width: '255px', height: '255px', backgroundColor: color }}
+      onMouseEnter={handleMouseEnter}
+      onDoubleClick={handleDoubleClick}
+    ></div>
+  );
+}
+export default ColorChanger;
+
+  
