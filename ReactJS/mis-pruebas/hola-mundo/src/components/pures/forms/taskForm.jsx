c02dbf2ba5b2f482a426daaa4316dfea2cc7b03c
hernanguaragna <hernanguaragna@gmail.com>
2023-01-27 16:04:57 -0300

    clear Inputs after add a task

--- ReactJS/mis-pruebas/hola-mundo/src/components/pures/forms/taskForm.jsx
+++ ReactJS/mis-pruebas/hola-mundo/src/components/pures/forms/taskForm.jsx
@@ -5,61 +5,70 @@ import { LEVELS } from '../../../models/levels.enum';
 import { Task } from '../../../models/task.class';
 
 const TaskForm = ({add,length}) => {
-    const nameRef = useRef('')
-    const descriptionRef = useRef('')
-    const levelRef = useRef(LEVELS.NORMAL)
+	const nameRef = useRef("");
+	const descriptionRef = useRef("");
+	const levelRef = useRef("LEVELS.NORMAL");
 
-    function addTask(e){
-        e.preventDefault()
-        const newTask = new Task(
-            nameRef.current.value,
-            descriptionRef.current.value,
-            false,
-            levelRef.current.value,
-
-        )
-        add(newTask)
-    }
-  
-
-    return (
-			<form
-				onSubmit={addTask}
-				className="d-flex justify-content-center align-items-center mb-4">
-				<div className="form-outline flex-fill">
-					<input
-						ref={nameRef}
-						id="inputName"
-						type="text"
-						className="form-control form-control-lg mt-2 mb-1"
-						required
-						autoFocus
-						placeholder="Task Name"
-					/>
-					<input
-						ref={descriptionRef}
-						id="inputDescription"
-						type="text"
-						className="form-control form-control-lg mb-1"
-						required
-						placeholder="Task description"
-					/>
-					{/* <label htmlFor="selectLevel" className='se-only'>Level</label> */}
-					<select
-						className="form-control form-control-lg mb-1 br-2"
-						ref={levelRef}
-						defaultValue={LEVELS.NORMAL}
-						id="selectLevel">
-						<option  value={LEVELS.NORMAL}>Normal</option>
-						<option value={LEVELS.URGENT}>Urgent</option>
-						<option value={LEVELS.BLOCKING}>Blocking</option>
-					</select>
-					<button type="submit" className="btn btn-success btn-lg ms-2 mt-2">
-						{length > 0 ? "Add Another Task" : "Create your first Task"}
-					</button>
-				</div>
-			</form>
+	function addTask(e) {
+		e.preventDefault();
+		const newTask = new Task(
+			nameRef.current.value,
+			descriptionRef.current.value,
+			false,
+			levelRef.current.value,
 		);
+		add(newTask);
+        clearInputs();
+	}
+	//funcion que limpie los inputs
+
+   function clearInputs() {
+    nameRef.current.value = '';
+    descriptionRef.current.value = '';
+    levelRef.current.value = LEVELS.NORMAL;
+}
+
+        
+	
+
+	return (
+		<form
+			onSubmit={addTask}
+			className="d-flex justify-content-center align-items-center mb-4">
+			<div className="form-outline flex-fill">
+				<input
+					ref={nameRef}
+					id="inputName"
+					type="text"
+					className="form-control form-control-lg mt-2 mb-1"
+					required
+					autoFocus
+					placeholder="Task Name"
+				/>
+				<input
+					ref={descriptionRef}
+					id="inputDescription"
+					type="text"
+					className="form-control form-control-lg mb-1"
+					required
+					placeholder="Task description"
+				/>
+				{/* <label htmlFor="selectLevel" className='se-only'>Level</label> */}
+				<select
+					className="form-control form-control-lg mb-1 br-2"
+					ref={levelRef}
+					defaultValue={LEVELS.NORMAL}
+					id="selectLevel">
+					<option value={LEVELS.NORMAL}>Normal</option>
+					<option value={LEVELS.URGENT}>Urgent</option>
+					<option value={LEVELS.BLOCKING}>Blocking</option>
+				</select>
+				<button type="submit" className="btn btn-success btn-lg ms-2 mt-2">
+					{length > 0 ? "Add Another Task" : "Create your first Task"}
+				</button>
+			</div>
+		</form>
+	);
 }
 TaskForm.prototype = {
 	add: PropTypes.func.isRequired,
