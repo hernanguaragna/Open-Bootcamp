cd4157fbaae93911e65961831b8954845a0b8cdb
hernanguaragna <hernanguaragna@gmail.com>
2023-02-02 14:52:09 -0300

    add user profiles

---
+++ ReactJS/mis-pruebas/hola-mundo/src/pages/profiles/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfilePage = ({user}) => {
+    return (
+        <div>
+            <h1>Your Profiles</h1>
+        </div>
+    );
+}
+
+export default ProfilePage;
