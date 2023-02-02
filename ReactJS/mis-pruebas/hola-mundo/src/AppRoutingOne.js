1dddc03d08afa6269ee71481941095b63755de66
hernanguaragna <hernanguaragna@gmail.com>
2023-02-02 14:21:30 -0300

    some changes

--- ReactJS/mis-pruebas/hola-mundo/src/AppRoutingOne.js
+++ ReactJS/mis-pruebas/hola-mundo/src/AppRoutingOne.js
@@ -1,18 +1,29 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import NotFoundPage from './pages/404/NotFoundPage';
+import AboutPage from './pages/about-faqs/AboutPage';
 
 function AppRoutingOne() {
 
   return (
 		<Router>
+			<div>
+				<aside>
+					<Link to="/">| HOME|</Link>
+					<Link to="/about">| ABOUT|</Link>
+					<Link to="/faqs">| FAQs|</Link>
+				</aside>
+				<main>
 			<Routes>
-				<Route path='/' element = {<HomePage />} />
+					<Route path="/" element={<HomePage />} />
+					<Route path="/about" element={<AboutPage />} />
+					<Route path="/faqs" element={<AboutPage />} />
 
-
-				{/* Dejo siempre al final el ERROR 404 */}
-				<Route path= '*' element = { <NotFoundPage /> } />
+					<Route path="*" element={<NotFoundPage />} />
 			</Routes>
+				</main>
+
+			</div>
 		</Router>
 	);
 }
--- ReactJS/mis-pruebas/hola-mundo/src/pages/about-faqs/AboutPage.jsx
+++ ReactJS/mis-pruebas/hola-mundo/src/pages/about-faqs/AboutPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {useNavigate, useLocation} from 'react-router-dom';
+
 
 const AboutPage = () => {
+    
+    // const location = useLocation();
+    const navigate = (path) => useNavigate.push(path);
+    const back = () => navigate(-1);
+    
     return (
-        <div>
-            <h1>About | Faq</h1>
-        </div>
-    );
+			<div>
+				<h1>About | FAQs</h1>
+				<div>
+					<button className="btn btn-primary">Go to Home</button>
+					<button className="btn btn-primary" onClick={back}>
+						Go Back
+					</button>
+				</div>
+			</div>
+		);
 }
 
 export default AboutPage;
