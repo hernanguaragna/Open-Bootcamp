5adfd700a26d58c08a12c586321e266da4de8626
hernanguaragna <hernanguaragna@gmail.com>
2023-02-02 14:48:35 -0300

    foward etc

--- ReactJS/mis-pruebas/hola-mundo/src/pages/about-faqs/AboutPage.jsx
+++ ReactJS/mis-pruebas/hola-mundo/src/pages/about-faqs/AboutPage.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import {useNavigate, useLocation} from 'react-router-dom';
+import {useHistory, useLocation} from 'react-router-dom';
 
 
 const AboutPage = () => {
     
     // const location = useLocation();
-    const navigate = (path) => useNavigate.push(path);
-    const back = () => navigate(-1);
+    const Navigate = (path) => {
+			history.push(path);
+		}
+    const back = () => Navigate(-1);
     
     return (
 			<div>
 				<h1>About | FAQs</h1>
 				<div>
-					<button className="btn btn-primary">Go to Home</button>
+					<button className="btn btn-primary" onClick ={()=>Navigate("/home")}>Go to Home</button>
 					<button className="btn btn-primary" onClick={back}>
 						Go Back
 					</button>
