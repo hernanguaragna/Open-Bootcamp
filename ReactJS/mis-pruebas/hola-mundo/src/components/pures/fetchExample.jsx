8cf4c0d18823e8fb00d52a2938504b84c4bf861a
hernanguaragna <hernanguaragna@gmail.com>
2023-02-13 16:01:49 -0300

    fetch funtional

--- ReactJS/mis-pruebas/hola-mundo/src/components/pures/fetchExample.jsx
+++ ReactJS/mis-pruebas/hola-mundo/src/components/pures/fetchExample.jsx
@@ -1,19 +1,26 @@
 import React, {useState,useEffect} from 'react';
-import { getAllUsers } from '../../services/fetchService';
+import { getAllPagedUsers, getAllUsers, getUserDetails } from '../../services/fetchService';
 const FetchExample = () => {
-    const [users, setUsers] = useState(0);
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectdUser] = useState({});
+    const [totalUsers, setTotalUsers] = useState(12);
+    const [usersPerPages, setUsersPerPages] = useState(6);
+    const [pages, setpages] = useState(2);
 
     useEffect(() => {
         
         return () => {
-            obteinUsers()
+            obtainUsers()
         };
     }, []);
-    const obteinUsers = ()=>{
+    const obtainUsers = ()=>{
         getAllUsers()
         .then((response)=>{
             console.log('All users', response.data)
             setUsers(response.data)
+            setpages(response.total_pages)
+            setUsersPerPages(response.per_page);
+            setTotalUsers(response.total);
         })
         .catch((error) =>{
             alert(`Error while retreiving the users ${error}`)
@@ -24,14 +31,68 @@ const FetchExample = () => {
         })
 
     }
+//////////////////////
+const obtainPagedUsers = (pages)=>{
+    getAllPagedUsers(pages)
+    .then((response)=>{
+        console.log('All Paged users', response.data)
+        setUsers(response.data)
+        setpages(response.total_pages)
+        setUsersPerPages(response.per_page);
+        setTotalUsers(response.total);
+    })
+    .catch((error) =>{
+        alert(`Error while retreiving the users ${error}`)
+    })
+    .finally(() =>{
+        console.log('Ended obtaining users')
+        console.table(users)
+    })
+
+}
+///////////////////
+
+const obtainUserDetails = (id)=>{
+    getUserDetails(id)
+    .then((response)=>{
+        console.log('All Paged users', response.data)
+        setSelectdUser(response.data)
+        
+    })
+    .catch((error) =>{
+        alert(`Error while retreiving the user ${error}`)
+    })
+    .finally(() =>{
+        console.log('Ended obtaining user')
+        console.table(selectedUser)
+    })
+}
+
+
     return (
-        <div>
-            
-            <h2>
-                
-            </h2>
-        </div>
-    );
+			<div>
+				<h2>Users:</h2>
+				{users.map((user, index) => {
+					return <p key={index} onClick={() => obtainUserDetails(user.id)}></p>;
+				})}
+				<p>
+					Showing {usersPerPages} users of {totalUsers} in total
+				</p>
+				<button onClick={() => obtainPagedUsers(1)}>1</button>
+				<button onClick={() => obtainPagedUsers(1)}>2</button>
+				<div>
+					<h3>User Details</h3>
+					{selectedUser && (
+						<div>
+							<p>Name: {selectedUser.first_name}</p>
+							<p>Last Name: {selectedUser.last_name}</p>
+							<p>Email: {selectedUser.email}</p>
+							<img src="{selectedUser.avatar}" alt="Avatar" style={{width:'50px',height:'50px'}}/>
+						</div>
+					)}
+				</div>
+			</div>
+		);
 }
 
 export default FetchExample;
--- ReactJS/mis-pruebas/hola-mundo/src/services/fetchService.js
+++ ReactJS/mis-pruebas/hola-mundo/src/services/fetchService.js
@@ -1,5 +1,19 @@
 export const getAllUsers = async () =>{
+    
     let response = await fetch('https://reqres.in/api/users')
     //we return the json
     return response.json()
 }
+export const getAllPagedUsers = async (pages) =>{
+
+    let response = await fetch(`https://reqres.in/api/users?pages=${pages}`)
+    //we return the json
+    return response.json()
+}
+export const getUserDetails = async (id) =>{
+
+    let response = await fetch(`https://reqres.in/api/users/${id}`)
+    //we return the json
+    return response.json()
+}
+ 
